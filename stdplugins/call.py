86775f251e4200693612da49c9ad95c4069aafe1
Ganjedii <58940977+Ganjedii@users.noreply.github.com>
2020-01-04 21:19:00 +0530

    Update call.py

--- stdplugins/call.py
+++ stdplugins/call.py
@@ -1,3 +1,18 @@
+"""
+Available Commands
+.call"""
+
+from telethon import events
+
+import asyncio
+
+
+
+
+
+
+
+
 @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 
 async def _(event):
